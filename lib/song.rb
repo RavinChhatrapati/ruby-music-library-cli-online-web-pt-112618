ea0f1ea6b518b69ad859f2c8ffb80c4a3cb27755
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-23 02:44:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
 @@all = []
 attr_accessor :name
-def initialize(name, artist = nil) # makes artist as an optional argument when set to artist = nil
+def initialize(name, artist = nil) # makes artist as an optional argument when set to artist = 
   @name = name
   self.artist = artist if artist
 end
@@ -20,18 +20,16 @@ def self.create(name)
 end
 end
 def artist=(artist)
-  if @artist = nil
+  if @artist == nil
     @artist = artist
   else
     @artist = artist
 end
 end
-if self.artist != nil
-      @artist.add_song(self)
-    end
-    @artist
-  end
+
+
+
+
 def artist
   @artist
-end
 end
